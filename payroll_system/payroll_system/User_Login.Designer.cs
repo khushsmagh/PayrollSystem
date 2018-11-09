a55607ba8bfa147a51289836934e3cd7d20c7959
sandeep saini <s.saini516@mybvc.ca>
2018-11-08 19:29:49 -0700

    authenticate the user

--- payroll_system/payroll_system/User_Login.Designer.cs
+++ payroll_system/payroll_system/User_Login.Designer.cs
@@ -40,35 +40,40 @@
             // 
             // loginButton
             // 
-            this.loginButton.Location = new System.Drawing.Point(135, 400);
+            this.loginButton.Location = new System.Drawing.Point(101, 325);
+            this.loginButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.loginButton.Name = "loginButton";
-            this.loginButton.Size = new System.Drawing.Size(146, 65);
+            this.loginButton.Size = new System.Drawing.Size(110, 53);
             this.loginButton.TabIndex = 0;
             this.loginButton.Text = "Login";
             this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
             // 
             // splitter1
             // 
             this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(3, 649);
+            this.splitter1.Size = new System.Drawing.Size(2, 527);
             this.splitter1.TabIndex = 1;
             this.splitter1.TabStop = false;
             // 
             // clockOutButton
             // 
-            this.clockOutButton.Location = new System.Drawing.Point(693, 489);
+            this.clockOutButton.Location = new System.Drawing.Point(520, 397);
+            this.clockOutButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.clockOutButton.Name = "clockOutButton";
-            this.clockOutButton.Size = new System.Drawing.Size(146, 65);
+            this.clockOutButton.Size = new System.Drawing.Size(110, 53);
             this.clockOutButton.TabIndex = 2;
             this.clockOutButton.Text = "Clock-out";
             this.clockOutButton.UseVisualStyleBackColor = true;
             // 
             // clockInButton
             // 
-            this.clockInButton.Location = new System.Drawing.Point(693, 330);
+            this.clockInButton.Location = new System.Drawing.Point(520, 268);
+            this.clockInButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.clockInButton.Name = "clockInButton";
-            this.clockInButton.Size = new System.Drawing.Size(146, 65);
+            this.clockInButton.Size = new System.Drawing.Size(110, 53);
             this.clockInButton.TabIndex = 3;
             this.clockInButton.Text = "Clock-in";
             this.clockInButton.UseVisualStyleBackColor = true;
@@ -77,9 +82,10 @@
             // 
             this.usernameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.usernameTextBox.ForeColor = System.Drawing.Color.Silver;
-            this.usernameTextBox.Location = new System.Drawing.Point(116, 136);
+            this.usernameTextBox.Location = new System.Drawing.Point(87, 110);
+            this.usernameTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.usernameTextBox.Name = "usernameTextBox";
-            this.usernameTextBox.Size = new System.Drawing.Size(196, 34);
+            this.usernameTextBox.Size = new System.Drawing.Size(148, 29);
             this.usernameTextBox.TabIndex = 4;
             this.usernameTextBox.Text = "Username";
             this.usernameTextBox.Enter += new System.EventHandler(this.usernameTextBox_Enter);
@@ -89,9 +95,10 @@
             // 
             this.passwordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.passwordTextBox.ForeColor = System.Drawing.Color.Silver;
-            this.passwordTextBox.Location = new System.Drawing.Point(116, 276);
+            this.passwordTextBox.Location = new System.Drawing.Point(87, 224);
+            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.passwordTextBox.Name = "passwordTextBox";
-            this.passwordTextBox.Size = new System.Drawing.Size(196, 34);
+            this.passwordTextBox.Size = new System.Drawing.Size(148, 29);
             this.passwordTextBox.TabIndex = 5;
             this.passwordTextBox.Text = "Password";
             this.passwordTextBox.Enter += new System.EventHandler(this.passwordTextBox_Enter);
@@ -101,9 +108,10 @@
             // 
             this.employeeNumber.AutoSize = true;
             this.employeeNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.employeeNumber.Location = new System.Drawing.Point(659, 125);
+            this.employeeNumber.Location = new System.Drawing.Point(494, 102);
+            this.employeeNumber.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.employeeNumber.Name = "employeeNumber";
-            this.employeeNumber.Size = new System.Drawing.Size(215, 29);
+            this.employeeNumber.Size = new System.Drawing.Size(170, 24);
             this.employeeNumber.TabIndex = 7;
             this.employeeNumber.Text = "Employee Number";
             // 
@@ -111,9 +119,10 @@
             // 
             this.employeeNumberTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.employeeNumberTextBox.ForeColor = System.Drawing.Color.Silver;
-            this.employeeNumberTextBox.Location = new System.Drawing.Point(664, 200);
+            this.employeeNumberTextBox.Location = new System.Drawing.Point(498, 162);
+            this.employeeNumberTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.employeeNumberTextBox.Name = "employeeNumberTextBox";
-            this.employeeNumberTextBox.Size = new System.Drawing.Size(196, 34);
+            this.employeeNumberTextBox.Size = new System.Drawing.Size(148, 29);
             this.employeeNumberTextBox.TabIndex = 8;
             this.employeeNumberTextBox.Text = "12345";
             this.employeeNumberTextBox.Enter += new System.EventHandler(this.employeeNumberTextBox_Enter);
@@ -121,9 +130,9 @@
             // 
             // User_Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1063, 649);
+            this.ClientSize = new System.Drawing.Size(797, 527);
             this.Controls.Add(this.employeeNumberTextBox);
             this.Controls.Add(this.employeeNumber);
             this.Controls.Add(this.passwordTextBox);
@@ -132,6 +141,7 @@
             this.Controls.Add(this.clockOutButton);
             this.Controls.Add(this.splitter1);
             this.Controls.Add(this.loginButton);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "User_Login";
             this.Text = "User_Login";
             this.Load += new System.EventHandler(this.User_Login_Load);
