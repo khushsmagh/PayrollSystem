4bc9453f3b8da3c32abe3c8a0a7157df7b68e0dc
j.laurilla280 <j.laurilla280@mybvc.ca>
2018-11-21 15:54:13 -0700

    Made data connection.

---
+++ payroll_system/payroll_databse/InsertValuesPayroll.sql
@@ -0,0 +1,35 @@
+INSERT INTO PR_Employee ( FirstName, LastName, DOB, DateHired, Position, HourlyRate)
+VALUES ('Khushveen', 'Smagh', '1991-03-04', '2018-10-15', 'Admin', 20),
+('Sandeep', 'Saini', '1991-10-24', '2018-10-15', 'Stocker', 17),
+('Hai', 'Do', '1986-11-20', '2018-09-11', 'Cashier', 25),
+('Pablo', 'Winter', '1999-05-18', '2018-10-15', 'Stocker', 17),
+('Brijesh', 'Patel', '1988-02-02', '2018-10-15', 'Stocker', 20),
+('Anderson', 'Resende Viana', '1975-07-14', '2018-10-15', 'Guard', 21),
+('Karamullah', 'Agha', '1995-10-24', '2018-10-15', 'Stocker', 17),
+('Nico', 'Beekhuijs', '1970-03-09', '2018-09-01', 'Supervisor', 10),
+('Parvir', 'Sandhu', '2001-01-22', '2018-10-15', 'Stocker', 17);
+
+INSERT INTO PR_User_Login (EmployeeId, UserName, [Password])
+VALUES (1, 'johnl', 'johnl'),
+(2, 'khushs', 'khushs'),
+(3, 'sandeeps', 'sandeeps'),
+(4, 'haid', 'haid'),
+(5, 'pablow', 'pablow'),
+(6, 'brijeshp', 'brijeshp'),
+(7, 'andersonr', 'andersonr'),
+(8, 'kara', 'kara'),
+(9, 'nicob', 'nicob'),
+(10, 'parvirs', 'parvirs');
+
+INSERT INTO PR_Schedule (EmployeeId, [Date], [Shift])
+VALUES (1, '2018-10-15', '0900-1700'),
+(3, '2018-10-15', '0900-1700'),
+(4, '2018-10-15', '1200-2000'),
+(1, '2018-10-16', '0800-1600'),
+(3, '2018-10-16', '0800-1600'),
+(1, '2018-10-17', '0900-1700'),
+(4, '2018-10-17', '0900-1700');
+
+
+INSERT INTO PR_Timesheet (EmployeeId, [Date], CLockInTime, ClockOutTime, TotalHours)
+VALUES (1, '2018-10-15', '09:00:00', '17:00:00', 8);
--- payroll_system/payroll_system/Properties/Settings.Designer.cs
+++ payroll_system/payroll_system/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace payroll_system.Properties
-{
-
-
+namespace payroll_system.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=199.103.60.77;Initial Catalog=retailmgmt;Persist Security Info=True;U" +
+            "ser ID=retailmgmt;Password=dgQDR9q8THTgalP")]
+        public string retailmgmtConnectionString {
+            get {
+                return ((string)(this["retailmgmtConnectionString"]));
             }
         }
     }
